d7f5070174a7b73aae5893715911c0283e4a0901
rpgarde <rpgarde@gmail.com>
2021-08-22 12:26:01 +1000

    add department functionality done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require("inquirer");
 const mysql = require('mysql2');
+const cTable = require('console.table');
 require('dotenv').config();
 
 const db = mysql.createConnection(
@@ -12,14 +13,14 @@ const db = mysql.createConnection(
     console.log(`Connected to the books_db database.`)
   );
 
-// const validator = (input) => {
-//     if (input.length === 0) {
-//         return 'Please provide an input';
-//     }
-//     else {
-//         return true;
-//     }
-// }
+const validator = (input) => {
+    if (input.length === 0) {
+        return 'Please provide an input';
+    }
+    else {
+        return true;
+    }
+}
 
 // const numValidator = (input) => {
 //     if (isNaN(input)) {
@@ -45,27 +46,90 @@ const introQuestions = [
     }
 ]
 
+const addDepartmentQuestion = [
+    {
+        type:"input",
+        message:"What is the name of the department you would like to add?",
+        name:"departmentName",
+        validate:validator
+    }
+]
+
+//Function to view all depts
+const viewDepartments = () => {
+    console.log("Here are all the departments:")
+  db.query('SELECT * FROM department', function (err, results) {
+    console.table(results)
+  });
+}
+//View all roles
+const viewRoles = () => {
+    console.log("Here are all the roles:")
+  db.query('SELECT * FROM role', function (err, results) {
+    console.table(results)
+  });
+}
+
+const addDepartment = () => {
+    inquirer.prompt(addDepartmentQuestion)
+        .then((data) => {
+            db.query(`INSERT INTO department (name) VALUES (?)`, data.departmentName, (err, results) => {
+                if (err) {
+                    console.log(err);
+                }
+                console.log(results);
+                console.log("Success, here are all the departments")
+                db.query('SELECT * FROM department', function (err, results) {
+                    console.table(results)
+                })
+            })
+        })
+}
+
 // init function
 const init = () => {
     console.log("Welcome to the employee tracker!")
     inquirer.prompt(introQuestions)
     .then((data)=>{
-        console.log(`${data.intro} is being asked`)
+        //switch statement 
+        switch (data.intro){
+            case "View all departments":
+                viewDepartments();
+                break;
+            case "View all roles":
+                viewRoles();
+                break;
+            case "Add a department":
+                addDepartment();
+                break;
+                // "Add a role",
+            // "Add an employee",
+            // "Update an employee role",
+            case "Exit application":
+                console.log("Thank you. Goodbye!")
+                process.exit();        
+            }
+        // console.log(`${data.intro} is being asked`)
     })
+    // .then(init)
     .catch((err)=>console.log(err))
 }
 init()
 
 // styling upon init 
 
-// Inquirer list with the following options
-    // View all departments
-    // View all roles
-    // Add a department 
-    // Add a role
-    // Add an employee 
-    // Update an employee role
-    // End application
+// db.query(`DELETE FROM favorite_books WHERE id = ?`, deletedRow, (err, result) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log(result);
+//   });
+  
+//   // Query database
+//   db.query('SELECT * FROM favorite_books', function (err, results) {
+//     console.log(results);
+//   });
+
 
 // When I choose view all departments
 // Then a table with department names and dept IDs
